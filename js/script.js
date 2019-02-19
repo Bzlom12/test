48b92a9c155a6dda48b3c0445d89fab3cd96ff39
Vlad <tixon16@rambler.ru>
2019-02-19 21:17:30 +0300

    commit 4

--- js/script.js
+++ js/script.js
@@ -31,6 +31,9 @@ window.addEventListener('DOMContentLoaded', () => {
 				empty = cartWrapper.querySelector('.empty');
 			trigger.remove();
 
+			showConfirm();
+			calcGoods(1);
+
 			removeBtn.classList.add('goods__item-remove');
 			removeBtn.innerHTML = '&times';
 			item.appendChild(removeBtn);
@@ -39,7 +42,64 @@ window.addEventListener('DOMContentLoaded', () => {
 			if (empty) {
 				empty.remove();
 			}
+
+			calcTotal();
+			removeFromCart();
 		});
 	});
+
+	function sliceTitle() {
+		titles.forEach(function(item) {
+		if (item.textContent.length < 70) {
+			return;
+		} else {
+			const str = item.textContent.slice(0, 71) + '...';
+			item.textContent = str;
+			}
+		});
+	}
+	sliceTitle();
+
+	function showConfirm() {
+		confirm.style.display = 'block';
+		let counter = 100;
+		const id = setInterval(frame, 10);
+		function frame() {
+			if (counter == 10) {
+				clearInterval(id);
+			} else {
+				counter--;
+				confirm.style.transform = `translateY(-${counter}px)`;
+				confirm.style.opacity = '.' + counter;
+			}
+		}
+	}
+	// setInterval запускается и повторяется через определенное время
+	// setTimeout(sliceTitle, 100) запускаем через какоето время
+
+	function calcGoods(i) {
+		const items = cartWrapper.querySelectorAll('.goods__item');
+		badge.textContent = i + items.length;
+	}
+
+	function calcTotal() {
+		const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span')
+		let total = 0;
+		prices.forEach(function(item) {
+			total += +item.textContent;
+		});
+		totalCost.textContent = total;
+	}
+
+	function removeFromCart () {
+		const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+		removeBtn.forEach(function(btn) {
+			btn.addEventListener('click', () => {
+				btn.parentElement.remove();
+				calcGoods(0);
+				calcTotal();
+			});
+		});
+	}
 });
 
